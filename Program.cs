d3f0d7086d8f5bd8e1fbb2aafc158d462ed86335
Anna <sviridova_anya@mail.ru>
2022-11-28 21:31:20 +0300

    Программа, которая по заданным координатам выводит номер квадрата

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿// 
+Console.Clear();
+int DataEntryXY(string str)
+{
+    Console.Write(str);
+    int number = int.Parse(Console.ReadLine());
+    return number;
+}
+void FindQandrat(int x, int y)
+{
+    if ((x > 0)&&(y>0)) {Console.WriteLine("Это первый квадрат");}
+    if ((x < 0)&&(y>0)) {Console.WriteLine("Это второй квадрат");}
+    if ((x < 0)&&(y<0)) {Console.WriteLine("Это третий квадрат");}
+    if ((x > 0)&&(y<0)) {Console.WriteLine("Это четвертый квадрат");}
+}
+
+int x = DataEntryXY("Введите координаты x = ");
+//Console.WriteLine(x);
+int y = DataEntryXY("Введите координаты y = ");
+//Console.WriteLine(y);
+FindQandrat(x,y);
